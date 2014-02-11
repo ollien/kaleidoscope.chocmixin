733dd7bfdcb55efd521c945b5d3dd8a6e44df894
Franz Heidl <franz@franzheidl.de>
2014-02-11 10:42:05 +0100

    Clean up.

--- init.js
+++ init.js
@@ -1,7 +1,16 @@
+/*
+Kaleidoscope-Chocmixin.
+A mixin for Chocolat to diff open files in Kaleidoscope.
+https://github.com/franzheidl/kaleidoscope.chocmixin
+Franz Heidl 2014
+MIT License
+*/
+
+
 var sys = require('sys');
 var exec = require('child_process').exec;
-
 var cmd = 'osascript -e \'tell application "System Events" to return POSIX path of (path to application "Kaleidoscope") as text\'';
+
 
 function execute(command, callback) {
     exec(command, function(error, stdout, stderr){ callback(stdout); });
@@ -9,13 +18,13 @@ function execute(command, callback) {
 
 
 function diffInKaleidoscope(files) {
-  // Alert.show('Diffing: ', JSON.stringify(files));
   execute('open -a Kaleidoscope ' + files, false);
 }
 
+
 function run() {
   
-  // collect files from split view and create space-separated argument string:
+  // Collect open files from current tab:
   var docs = Tab.current().visibleDocuments();
   var files = [];
   for (var i = 0; i < docs.length; i++) {
@@ -33,7 +42,7 @@ function run() {
   });
 }
 
+
 Hooks.addMenuItem('Go/Diff in Kaleidoscope', 'cmd-ctrl-k', function() {
   run();
 });
-
